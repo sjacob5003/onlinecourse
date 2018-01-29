1e0e872a84fddf675b603ba3011098f2420b6121
Haresh Samnani <haresh.samnani@gmail.com>
2018-01-29 12:08:36 +0530

    formatted code
    Formatted code and removed un ncessary whitespaces

--- index.php
+++ index.php
@@ -2,20 +2,20 @@
 session_start();
 error_reporting(0);
 include("includes/config.php");
-if(isset($_POST['submit']))
-{
+  if(isset($_POST['submit']))
+  {
     $regno=$_POST['regno'];
     $password=md5($_POST['password']);
-$query=mysql_query("SELECT * FROM students WHERE StudentRegno='$regno' and password='$password'");
-$num=mysql_fetch_array($query);
-if($num>0)
-{
-$extra="change-password.php";//
-$_SESSION['login']=$_POST['regno'];
-$_SESSION['id']=$num['studentRegno'];
-$_SESSION['sname']=$num['studentName'];
-$uip=$_SERVER['REMOTE_ADDR'];
-$status=1;
+    $query=mysql_query("SELECT * FROM students WHERE StudentRegno='$regno' and password='$password'");
+    $num=mysql_fetch_array($query);
+      if($num>0)
+        {
+          $extra="change-password.php";//
+          $_SESSION['login']=$_POST['regno'];
+          $_SESSION['id']=$num['studentRegno'];
+          $_SESSION['sname']=$num['studentName'];
+          $uip=$_SERVER['REMOTE_ADDR'];
+          $status=1;
 $log=mysql_query("insert into userlog(studentRegno,userip,status) values('".$_SESSION['login']."','$uip','$status')");
 $host=$_SERVER['HTTP_HOST'];
 $uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
@@ -39,9 +39,6 @@ exit();
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-
     <title>Student Login</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
@@ -54,9 +51,7 @@ exit();
             <div class="row">
                 <div class="col-md-12">
                     <h4 class="page-head-line">Login To Enter </h4>
-
                 </div>
-
             </div>
              <span style="color:red;" ><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
             <form name="admin" method="post">
