6670391475901193f5e9707468c1a85896a826b3
Sourabh <sbhgeorge@gmail.com>
2018-02-09 10:56:44 +0530

    modified signup.php and homepage

--- index.php
+++ index.php
@@ -1,48 +1,8 @@
 <?php
 session_start();
-error_reporting(0);
-include("includes/config.php");
+include('includes/config.php');
 $host  = $_SERVER['HTTP_HOST'];
 $uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
-if(isset($_POST['submit']))
-{
-    $email=$_POST['email'];
-    $email=filter_var($email, FILTER_SANITIZE_EMAIL);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-    {
-        $_SESSION['errmsg']="Invalid Email";
-        header("Location:http://$host$uri/index.php");
-        exit();
-    }
-    else
-    {
-        $password=$_POST['password'];
-        $query=mysqli_query($con, "SELECT * FROM userstable WHERE UserEmail='$email' AND UserPassword='$password'");
-        $num=mysqli_fetch_array($query);
-        if($num>0)
-        {
-            $_SESSION['email']=$email;
-            $_SESSION['id']=$num['UserId'];
-            $_SESSION['sname']=$num['UserName'];
-            $_SESSION['utype']=$num['UserType'];
-            $uip=$_SERVER['REMOTE_ADDR'];
-            mysqli_query($con, "INSERT INTO userlog(UserId,IP) VALUES('".$_SESSION['id']."','$uip')");
-            if($_SESSION['utype']==1)
-                header("Location:http://$host$uri/my-profile.php");
-            else if($_SESSION['utype']==2)
-                header("Location:http://$host$uri/admin/index.php");
-            else if($_SESSION['utype']==3)
-                header("Location:http://$host$uri/change-password.php");
-            exit();
-        }
-        else
-        {
-            $_SESSION['errmsg']="Invalid Email/Password Combination";
-            header("Location:http://$host$uri/index.php");
-            exit();
-        }
-    }
-}
 ?>
 
 <!DOCTYPE html>
@@ -50,45 +10,30 @@ if(isset($_POST['submit']))
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <title>Student Login</title>
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Home</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <link href="assets/css/style.css" rel="stylesheet" />
 </head>
+
 <body>
-    <?php include('includes/header.php');?>
+<?php include('includes/header.php');
+  include('includes/menubar.php');
+?>
+    <!-- MENU SECTION END-->
     <div class="content-wrapper">
         <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                    <center><h4 class="page-head-line">Login To Enter </h4></center>
-                </div>
-            </div>
-             <span style="color:red;" ><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
-            <form name="admin" method="post">
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                     <label>Enter Email : </label>
-                        <input type="email" name="email" class="form-control"  />
-                        <label>Enter Password :  </label>
-                        <input type="password" name="password" class="form-control"  />
-                        <hr />
-                        <button type="submit" name="submit" class="btn btn-info"><span class="glyphicon glyphicon-user"></span> &nbsp;Log Me In </button>&nbsp;
-
-                        <p style="text-align:center;color:red;font-size:130%">New here? <a href="signup.php"> <u>Register now</u> </a></p>
-
-                </div>
-                </form>
-            </div>
+              <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-head-line">View Courses</h1>
+                    </div>
+                </div>                
         </div>
     </div>
-    <!-- CONTENT-WRAPPER SECTION END-->
-    <?php include('includes/footer.php');?>
-    <!-- FOOTER SECTION END-->
-    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-    <!-- CORE JQUERY SCRIPTS -->
+  <?php include('includes/footer.php');?>
     <script src="assets/js/jquery-1.11.1.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
     <script src="assets/js/bootstrap.js"></script>
 </body>
 </html>
