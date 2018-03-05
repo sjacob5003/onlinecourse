abfa8d227e9b5d9785fa605190f67d56655bc29f
Sourabh <sbhgeorge@gmail.com>
2018-03-05 12:23:15 +0530

    working enrolhistory

--- enrolhistory.php
+++ enrolhistory.php
@@ -14,7 +14,7 @@ if(strlen($_SESSION['userid'])!=NULL && $_SESSION['usertype']=="Student")
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Enroll History</title>
+    <title>Enrol History</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <link href="assets/css/style.css" rel="stylesheet" />
@@ -37,52 +37,42 @@ if(strlen($_SESSION['userid'])!=NULL && $_SESSION['usertype']=="Student")
                 <div class="col-md-12">
                     <!--    Bordered Table  -->
                     <div class="panel panel-default">
-                        <div class="panel-heading">
-                           Enroll History
-                        </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
                             <div class="table-responsive table-bordered">
                                 <table class="table">
                                     <thead>
                                         <tr>
-                                            <th>#</th>
+                                            <th>Course Code</th>
                                             <th>Course Name </th>
-                                            <th>Session </th>
-                                            <th> Department</th>
+                                            <th>Duration </th>
                                              <th>Level</th>
-                                                <th>Semester</th>
-                                             <th>Enrollment Date</th>
-                                             <th>Action</th>
+                                             <th>Marks/Grade</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-<?php
-$sql=mysql_query("select courseenrolls.course as cid, course.courseName as courname,session.session as session,department.department as dept,level.level as level,courseenrolls.enrollDate as edate ,semester.semester as sem from courseenrolls join course on course.id=courseenrolls.course join session on session.id=courseenrolls.session join department on department.id=courseenrolls.department join level on level.id=courseenrolls.level  join semester on semester.id=courseenrolls.semester  where courseenrolls.studentRegno='".$_SESSION['login']."'");
-$cnt=1;
-while($row=mysql_fetch_array($sql))
-{
-?>
+    <?php
+    $sql=mysqli_query($con, "SELECT CourseCode, CourseName, CourseLevel, CourseStartDate, CourseEndDate FROM coursetable JOIN coursedurationtable ON CourseDurationId=DurationId JOIN courseenrolmenttable ON coursetable.CourseId = courseenrolmenttable.CourseId WHERE StudentId=".$_SESSION['userid']);
+    while($row=mysqli_fetch_array($sql))
+    {
+    ?>
 
 
                                         <tr>
-                                            <td><?php echo $cnt;?></td>
-                                            <td><?php echo htmlentities($row['courname']);?></td>
-                                            <td><?php echo htmlentities($row['session']);?></td>
-                                            <td><?php echo htmlentities($row['dept']);?></td>
-                                            <td><?php echo htmlentities($row['level']);?></td>
-                                            <td><?php echo htmlentities($row['sem']);?></td>
-                                             <td><?php echo htmlentities($row['edate']);?></td>
-                                            <td>
-                                            <a href="print.php?id=<?php echo $row['cid']?>" target="_blank">
-<button class="btn btn-primary"><i class="fa fa-print "></i> Print</button> </a>                                        
-
-
-                                            </td>
+                                            <td><?php echo $row['CourseCode'];?></td>
+                                            <td><?php echo $row['CourseName'];?></td>
+                                            <td><?php echo $row['CourseStartDate']." - ".$row['CourseEndDate'];?></td>
+                                            <td><?php if($row['CourseLevel']==1)
+                                              echo "Beginner";
+                                              elseif($row['CourseLevel']==2)
+                                              echo "Intermediate";
+                                              elseif($row['CourseLevel']==3)
+                                              echo "Expert";
+                                                ?></td>
+                                            <td></td>
                                         </tr>
-<?php 
-$cnt++;
-} ?>
+    <?php 
+    } ?>
 
                                         
                                     </tbody>
@@ -93,11 +83,6 @@ $cnt++;
                      <!--  End  Bordered Table  -->
                 </div>
             </div>
-
-
-
-
-
         </div>
     </div>
     <!-- CONTENT-WRAPPER SECTION END-->
