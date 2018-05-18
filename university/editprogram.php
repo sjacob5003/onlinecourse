b4c5ef1c509d18e115ba3853704c1770839bdc48
Haresh Samnani <haresh.samnani@gmail.com>
2018-05-19 01:12:10 +0530

    created edit program page

--- university/editprogram.php
+++ university/editprogram.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('includes/config.php');
+include('../includes/config.php');
 $host  = $_SERVER['HTTP_HOST'];
 $uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
 if(strlen($_SESSION['email'])==NULL)
@@ -9,17 +9,8 @@ if(strlen($_SESSION['email'])==NULL)
 }
 else
 {
-  if(isset($_POST['submit']))
-  {
-    $feedbacksubject = $_POST['feedbacksubject'];
-    $feedbackdescription = $_POST['feedbackdescription'];
-    $feedbackcourseid = $_POST['feedbackcourseid'];
-    $studentid = $_SESSION['userid'];
-    if(mysqli_query($con,"INSERT INTO feedbacktable (CourseDurationId, FeedbackStudentId, FeedbackSubject, FeedbackDescription) VALUES ('$feedbackcourseid', '$studentid', '$feedbacksubject', '$feedbackdescription') "))
-      $_SESSION['msg'] = "Thank you for your feedback";
-    else
-      $_SESSION['msg'] = "Could not submit your feedback. Please try again";
-  }
+          //program edit code
+}
 ?>
 
 <!DOCTYPE html>
@@ -36,57 +27,113 @@ else
     <link href="assets/css/style.css" rel="stylesheet" />
 </head>
 <body>
-<?php include('includes/header.php');
+<?php include('../includes/header.php');
 if($_SESSION['email']!="")
-  include('includes/menubar.php');
+  include('../includes/menubar.php');
 ?>
     <!-- MENU SECTION END-->
     <div class="content-wrapper">
         <div class="container">
               <div class="row">
                     <div class="col-md-6 col-md-offset-3">
-                        <center><h1 class="page-head-line">Feedback Form</h1></center>
+                        <center><h1 class="page-head-line">Edit Program</h1></center>
                     </div>
                 </div>
                 <div class="row" >
-                  <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                      <div class="panel panel-default">
-                          <font color="green" align="center">
-                          <?php echo htmlentities($_SESSION['msg']);  
-                                $_SESSION['msg']="";?></font>
 
-                            <div class="panel-body">
-                            <form name="dept" method="post" enctype="multipart/form-data">
-
-                              <div class="form-group">
-                                <label>Select Course</label>
-                                  <select class="selectpicker" data-live-search="true" name="feedbackcourseid" data-style="btn" data-width="100%" data-border="1px" title="Select the Course" required>
-                                  <?php
-                                    $sql=mysqli_query($con, "SELECT programofferedtable.CourseId, coursetable.CourseName FROM programofferedtable JOIN coursetable ON coursetable.CourseId = programofferedtable.CourseId WHERE programofferedtable.ProgramId = 1");
-                                    while($row = mysqli_fetch_array($sql))
-                                    {
-                                    ?>
-                                      <option value=<?php echo $row['DurationId']; ?>><?php echo $row['CourseName']; ?></option>
-                                  <?php
-                                    }
-                                  ?>
-                                  </select>
-                                </div>                              
-                          <?php } ?>
-
-                             <button type="submit" name="submit" id="submit" style="width:100%" class="btn btn-default">Save/button>
-                            </form>
-                        </div>
-                      </div>
-                    </div>
-                </div>
-            </div>
+         		<div class="col-sm-4 col-sm-offset-1">
+                 <div class="list-group" id="list1">
+                 <a href="#" class="list-group-item active">Courses Available <input title="toggle all" type="checkbox" class="all pull-right"></a>
+                 <a href="#" class="list-group-item">Second item <input type="checkbox" class="pull-right"></a>
+                 <a href="#" class="list-group-item">Third item <input type="checkbox" class="pull-right"></a>
+                 <a href="#" class="list-group-item">More item <input type="checkbox" class="pull-right"></a>
+                 <a href="#" class="list-group-item">Another <input type="checkbox" class="pull-right"></a>
+                 </div>
+               </div>
+               <div class="col-md-2 v-center">
+                    <button title="Send to list 2" class="btn btn-default center-block add"><i class="glyphicon glyphicon-chevron-right"></i></button>&nbsp;
+                   <button title="Send to list 1" class="btn btn-default center-block remove"><i class="glyphicon glyphicon-chevron-left"></i></button>&nbsp;
+               </div>
+               <div class="col-sm-4">
+           	  <div class="list-group" id="list2">
+                 <a href="#" class="list-group-item active">Courses Chosen <input title="toggle all" type="checkbox" class="all pull-right"></a>
+                 <a href="#" class="list-group-item">Alpha <input type="checkbox" class="pull-right"></a>
+                 <a href="#" class="list-group-item">Charlie <input type="checkbox" class="pull-right"></a>
+                 <a href="#" class="list-group-item">Bravo <input type="checkbox" class="pull-right"></a>
+                 <a href="#" class="list-group-item">Beta <input type="checkbox" class="pull-right"></a>
+                 </div>
+               </div>
+               <div class="col-md-2 col-md-offset-5">
+                         <button type="submit" name="submit" id="submit" style="width:100%" class="btn btn-default">Save</button>
+               </div>
         </div>
     </div>
-  <?php include('includes/footer.php');?>
+</div>
+  <?php include('../includes/footer.php');?>
     <script src="assets/js/jquery-1.11.1.js"></script>
     <script src="assets/js/bootstrap-select.min.js"></script>
     <script src="assets/js/bootstrap.js"></script>
+    <script>
+          $('.add').click(function(){
+          $('.all').prop("checked",false);
+          var items = $("#list1 input:checked:not('.all')");
+          var n = items.length;
+              if (n > 0) {
+          items.each(function(idx,item){
+            var choice = $(item);
+            choice.prop("checked",false);
+            choice.parent().appendTo("#list2");
+          });
+              }
+          else {
+                        alert("Choose an item from list 1");
+          }
+          });
+
+          $('.remove').click(function(){
+          $('.all').prop("checked",false);
+          var items = $("#list2 input:checked:not('.all')");
+              items.each(function(idx,item){
+          var choice = $(item);
+          choice.prop("checked",false);
+          choice.parent().appendTo("#list1");
+          });
+          });
+
+          /* toggle all checkboxes in group */
+          $('.all').click(function(e){
+              e.stopPropagation();
+              var $this = $(this);
+          if($this.is(":checked")) {
+              $this.parents('.list-group').find("[type=checkbox]").prop("checked",true);
+          }
+          else {
+              $this.parents('.list-group').find("[type=checkbox]").prop("checked",false);
+            $this.prop("checked",false);
+          }
+          });
+
+          $('[type=checkbox]').click(function(e){
+          e.stopPropagation();
+          });
+
+          /* toggle checkbox when list group item is clicked */
+          $('.list-group a').click(function(e){
+
+          e.stopPropagation();
+
+              var $this = $(this).find("[type=checkbox]");
+          if($this.is(":checked")) {
+              $this.prop("checked",false);
+          }
+          else {
+              $this.prop("checked",true);
+          }
+
+          if ($this.hasClass("all")) {
+              $this.trigger('click');
+          }
+          });
+    </script>
 </body>
 </html>
