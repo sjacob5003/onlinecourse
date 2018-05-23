b65ac08c2c707b3dcfd2e779d6b1c428f16a9a05
Sourabh Jacob <sbhgeorge@gmail.com>
2018-05-23 09:57:40 +0530

    updated footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <b>&copy; 2018 Online Course Registration | By : <a href="http://www.nuv.ac.in" target="_blank">NUV</a></b>
+                    <b>&copy; 2018 | The Online School</b>
                 </div>
 
             </div>
